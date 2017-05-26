7666e98fb2bff37c0ad9750d5e1ecef77ad9c80e
Hugo Arregui <hugo.arregui@gmail.com>
2017-05-25 23:55:17 -0300

    Update CV

--- generator/cv.scm
+++ generator/cv.scm
@@ -16,152 +16,183 @@
     (and r
          (cadr r))))
 
+(define (ref-list key data)
+  (let ((r (assq key data)))
+    (and r
+         (cdr r))))
+
 (define (two-cols-section data title f #!key (left-col-size 3))
-  `(div (@ (class "keeptogether"))
-        (h3 ,title)
+  `(div (@
+         (class "section"))
+        (h2 ,title)
         (div
-          ,(fold-right
-             (lambda (data tail)
-               (let-values (((col1 col2) (f data)))
-                 (cons `(row (div (@ (class ,(string-append "col-" (number->string left-col-size) " text-right")))
-                                  ,col1)
-                             (div (@ (class ,(string-append "col-" (number->string (- 12 left-col-size)))))
-                                  ,col2))
-                       tail)))
-             '()
-             data))))
+         ,(fold-right
+           (lambda (data tail)
+             (let-values (((col1 col2) (f data)))
+               (cons `(row (div (@ (class "three columns left-col"))
+                                ,col1)
+                           (div (@ (class "nine columns"))
+                                ,col2))
+                     tail)))
+           '()
+           data))))
 
 (define (positions-block)
-  (two-cols-section (ref-cv-data 'positions)
-                    "Professional Experience"
-                    (lambda (position)
-                      (let* ((ref        (cut ref <> position))
-                             (company    (ref 'company))
-                             (website    (ref 'company-website))
-                             (end-date   (ref 'endDate))
-                             (start-date (ref 'startDate))
-                             (summary    (ref 'summary))
-                             (keywords   (ref 'keywords))
-                             (title      (ref 'title)))
-                        (values
-                          `(b ,start-date " - "
-                              ,(or end-date
-                                   '(span (@ (class "label label-success")) "[PRESENT]")))
-                          `((b ,title) " at " 
-                                       ,(if website
-                                         `(a (@ (href ,website) (class "company")) ,company)
-                                         `(span (@ (class "company")) ,company))
-                                       (p)
-                                       (p ,(or summary '()))
-                                       ,(if keywords
-                                          `(p (em "Keywords: ") ,(string-join keywords ", "))
-                                          '())))))
-                    left-col-size: 3))
+  (two-cols-section
+   (ref-cv-data 'positions)
+   "Professional Experience"
+   (lambda (position)
+     (let* ((ref        (cut ref <> position))
+            (summary    (ref-list 'summary position))
+            (company    (ref 'company))
+            (website    (ref 'company-website))
+            (end-date   (ref 'endDate))
+            (start-date (ref 'startDate))
+            (keywords   (ref 'keywords))
+            (title      (ref 'title)))
+       (values
+        `(b ,start-date " - "
+            ,(or end-date
+                 '(span (@ (class "present")) "PRESENT")))
+        `((b ,title) " at "
+          ,(if website
+               `(a (@ (href ,website) (class "company")) ,company)
+               `(span (@ (class "company")) ,company))
+          (p)
+          ,(map (lambda (s) `(p ,s)) (or summary '()))
+          ,(if keywords
+               `(p (em ,(string-join keywords ", ")))
+               '())))))
+   left-col-size: 3))
 
 (define (publications-block)
-  (two-cols-section (ref-cv-data 'publications)
-                    "Publications"
-                    (lambda (publication)
-                      (let* ((ref          (cut ref <> publication))
-                             (authors      (ref 'authors))
-                             (date         (ref 'date))
-                             (title        (ref 'title))
-                             (where        (ref 'where))
-                             (article-link (ref 'article-link)))
-                        (values
-                          `(p (b ,date))
-                          `(div
-                             (p (b ,title) ,(if article-link
-                                                 `(a (@ (href ,article-link)) (small "(read it)"))
-                                                 '()))
-                             (p ,where)
-                             (p (i ,authors))))))
-                    left-col-size: 3))
+  (two-cols-section
+   (ref-cv-data 'publications)
+   "Publications"
+   (lambda (publication)
+     (let* ((ref          (cut ref <> publication))
+            (authors      (ref 'authors))
+            (date         (ref 'date))
+            (title        (ref 'title))
+            (where        (ref 'where))
+            (article-link (ref 'article-link)))
+       (values
+        `(p (b ,date))
+        `(div
+          (p (b ,title) ,(if article-link
+                             `(a (@ (href ,article-link)) (small "(read it)"))
+                             '()))
+          (p ,where)
+          (p (i ,authors))))))
+   left-col-size: 3))
 
 (define (heducation-block)
-  (two-cols-section (ref-cv-data 'higher-education)
-                    "Higher Education"
-                    (lambda (education)
-                      (let* ((ref         (cut ref <> education))
-                             (institution (ref 'institution))
-                             (start-date  (ref 'startDate))
-                             (end-date    (ref 'endDate))
-                             (title       (ref 'title))
-                             (comments    (ref 'comments)))
-                        (values
-                          `(b ,start-date " - " ,(or end-date
-                                                     '(span (@ (class "label label-success" "[PRESENT]")))))
-                          `(span ,title " at " ,institution " " ,comments))))))
+  (two-cols-section
+   (ref-cv-data 'higher-education)
+   "Higher Education"
+   (lambda (education)
+     (let* ((ref         (cut ref <> education))
+            (institution (ref 'institution))
+            (start-date  (ref 'startDate))
+            (end-date    (ref 'endDate))
+            (title       (ref 'title))
+            (comments    (ref 'comments)))
+       (values
+        `(b ,start-date
+            " - "
+            ,(or end-date
+                 '(span (@ (class "label label-success" "[PRESENT]")))))
+        `(span ,title " at " ,institution " " ,comments))))))
 
 (define (other-education-block)
-  (two-cols-section (ref-cv-data 'other-education)
-                    "Other Education"
-                    (lambda (education)
-                      (let* ((ref   (cut ref <> education))
-                             (where (ref 'where))
-                             (title (ref 'title))
-                             (title-en (ref 'title-en))
-                             (date  (ref 'date))
-                             (desc  (ref 'desc)))
-                        (values `(b ,date " - " ,where) `(div 
-                                                              ,(if title-en
-                                                                 `(span ,title(em " - (" ,title-en ")"))
-                                                                 `(span ,title))))))))
-
-(define (skills-block)
-  (two-cols-section (ref-cv-data 'skills)
-                    "Skills"
-                    (lambda (skill)
-                      (let* ((name (car skill))
-                             (keys (cadr skill)))
-                        (values `(b ,name ":") (string-join keys ", "))))))
+  (two-cols-section
+   (ref-cv-data 'other-education)
+   "Other Education"
+   (lambda (education)
+     (let* ((ref   (cut ref <> education))
+            (where (ref 'where))
+            (title (ref 'title))
+            (title-en (ref 'title-en))
+            (date  (ref 'date))
+            (desc  (ref 'desc)))
+       (values `(b ,date " - " ,where)
+               `(div
+                 ,(if title-en
+                      `(span ,title(em " - (" ,title-en ")"))
+                      `(span ,title))))))))
 
 (define (personal-info-block)
-  `(
-    (itemize
-      "Birth date: December 2, 1986"
-      "Nationality: Argentina"
-      (url "mailto:hugo.arregui.laboral@gmail.com" "hugo.arregui.laboral@gmail.com")
-      (url "http://ar.linkedin.com/in/hugoarregui/" "LinkedIn Profile")
-      (url "https://github.com/hugoArregui" "GitHub Profile")
-      (url "http://code.google.com/u/hugo.arregui/" "Google Code Profile")
-      (url "https://www.openhub.net/accounts/hugo_arregui" "OpenHub Profile"))))
+  `((itemize
+     (@
+      (class "contact-information"))
+     "Birth date: December 2, 1986"
+     "Nationality: Argentina"
+     (url "mailto:hugo.arregui.laboral@gmail.com" "Email")
+     (url "http://ar.linkedin.com/in/hugoarregui/" "LinkedIn")
+     (url "https://github.com/hugoArregui" "GitHub")
+     (url "https://www.openhub.net/accounts/hugo_arregui" "OpenHub"))))
 
-(define (write-html sxml-content)
-  (sxml->html 
-    `((doctype-html)
-      (html
-        (head
-          (title "Hugo Arregui")
-          (meta (@ (http-equiv "Content-Type") (content "text/html; charset=utf-8")))
-          (css-link "css/bootstrap.css")
-          (css-link "css/bootstrap-glyphicons.css")
-          (css-link "css/site.css"))
-        (body
-          ,sxml-content
-          (js-link "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js")
-          (js-link "js/bootstrap.min.js"))))))
+(define (intro-block)
+  `((div
+     (p (u "Some things I like:"))
+     (itemize (@ (class "intro"))
+              ,@(ref-cv-data 'likes))
+     (p (u "Some things I would like to do:"))
+     (itemize (@ (class "intro"))
+              ,@(ref-cv-data 'to-do)))))
+
+(define (write-html dest sxml-content)
+  (sxml->html
+   `((doctype-html)
+     (html
+      (head
+       (title "Hugo Arregui")
+       (meta (@ (http-equiv "Content-Type") (content "text/html; charset=utf-8")))
+       (meta (@ (name "viewport") (content "width=device-width, initial-scale=1">)))
+       (link (@ (rel "shortcut icon") (type "image/x-icon") (href "favicon.ico")))
+       (link (@ (href "//fonts.googleapis.com/css?family=Raleway:400,300,600" (rel "stylesheet") (type "text/css"))))
+       (css-link "static/Skeleton-2.0.4/css/normalize.css")
+       (css-link "static/Skeleton-2.0.4/css/skeleton.css")
+       ,(if (eq? dest 'web)
+            '(css-link "static/css/site.css")
+            '(css-link "static/css/pdf.css")))
+      (body
+       ,sxml-content)))))
 
 (define (generate)
-  (with-output-to-file "cv.html" (cut write-string (write-html `(container
-                                                                  (page-header (h1 "Hugo Arregui"))
-                                                                  (row
-                                                                    (div (@ (class "col-9"))
-                                                                         ,(positions-block)
-                                                                         ,(publications-block)
-                                                                         ,(heducation-block)
-                                                                         ,(other-education-block)
-                                                                         ,(skills-block))
-                                                                    (div (@ (class "col-3"))
-                                                                         ,(personal-info-block)))))))
-  (with-output-to-file "cv-for-pdf.html" (cut write-string (write-html `(container
-                                                                          (h1 "Hugo Arregui")
-                                                                          (hr)
-                                                                          ,(personal-info-block)
-                                                                          ,(positions-block)
-                                                                          ,(publications-block)
-                                                                          ,(heducation-block)
-                                                                          ,(other-education-block)
-                                                                          ,(skills-block))))))
+  (with-output-to-file
+      "cv.html"
+    (cut write-string
+         (write-html 'web
+                     `(container
+                       (h1 "Hugo Arregui")
+                       (hr)
+                       (row
+                        (div (@ (class "nine columns"))
+                             ,(intro-block)
+                             ,(positions-block)
+                             ,(publications-block)
+                             ,(heducation-block)
+                             ,(other-education-block))
+                        (div (@ (class "three columns"))
+                             ,(personal-info-block)))))))
+  (with-output-to-file
+      "cv-for-pdf.html"
+    (cut write-string
+         (write-html 'pdf
+                     `(div
+                       (h1 "Hugo Arregui")
+                       (hr)
+                       (div
+                        (p "There is much nicer version of this CV " (a (@ (href "http://hugoarregui.github.io/cv.html")) "online"))
+                        (br)
+                        ,(personal-info-block)
+                        (br)
+                        ,(intro-block)
+                        (br)
+                        ,(positions-block)
+                        ,(publications-block)
+                        ,(heducation-block)
+                        ,(other-education-block)))))))
 
 (generate)
